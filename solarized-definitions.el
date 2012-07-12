74495a8399c6c4d50febb13860c4729a8c82b1cb
Abhishek Sarkar <aksarkar@mit.edu>
2012-07-12 12:26:22 -0400

    Escaped characters use escape-glyph, not escape-glyph-face

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -180,6 +180,7 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
              (default ((t (,@fg-base0 ,@bg-back)))) ; Normal
              (cursor ((t (,@fg-base03 ,@bg-base0)))) ; Cursor
              (error ((t (,@fmt-bold ,@fg-red)))) ; Error
+             (escape-glyph ((t (,@fg-red))))
              (escape-glyph-face ((t (,@fg-red))))
              (fringe ((t (,@fg-base01 ,@bg-base02))))
              (linum ((t (,@fg-base01 ,@bg-base02))))
